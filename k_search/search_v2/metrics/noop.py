2897880beb42e7287b920e827096ef8c6d0a5bd3
Garrett Goon <goon@ibm.com>
2026-03-05 08:26:02 +0000

    refactor: remove old search_v2 directory

--- k_search/search_v2/metrics/noop.py
+++
@@ -1,5 +0,0 @@
-class NoOpMetricsTracker:
-    """No-op implementation of MetricsTracker for when metrics aren't needed."""
-
-    def log(self, metrics: dict[str, float | int], step: int | None = None) -> None:
-        pass
